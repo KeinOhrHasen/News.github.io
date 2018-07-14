2de085e1941c3a5215d18eca5c78d21815aceab8
KeinOhrHasen <34781917+KeinOhrHasen@users.noreply.github.com>
2018-07-14 14:15:17 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 //  download the JSON file
 window.onload = () =>{
-//     fetch("https://api.myjson.com/bins/140vry")
-    fetch("file://db/db.json")
-//        fetch("https://keinohrhasen.github.io/News.github.io/db/db.json")
+    fetch("https://api.myjson.com/bins/140vry")
+//     fetch("file://db/db.json")
+//     fetch("https://keinohrhasen.github.io/News.github.io/db/db.json")
 
     
     .then(response => {
       response.json().then(data => {
-        console.log(data);
+//         console.log(data);
         // store data in localStorage
         const myJSON = JSON.stringify(data);
         localStorage.setItem("testJSON", myJSON);
